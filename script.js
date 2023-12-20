847503aabc62d743be4c0533162238596800ecba
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-20 13:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-function isSameType(value1, value2) {
-	//your js code here
+// function isSameType(value1, value2) {
+// 	//your js code here
   
-	  if(isNaN(value1) && isNaN(value2)){
-		  return true;
-	  }
+// 	  if(isNaN(value1) && isNaN(value2)){
+// 		  return true;
+// 	  }
   
-	  return typeof value1===typeof value2;
+// 	  return typeof value1===typeof value2;
 	  
-  }
+//   }
 
-  // do not change the code below.
-  let value1 = prompt("Enter Start of the Range.");
-  let value2 = prompt("Enter End Of the Range.");
+
+
+//   // do not change the code below.
+//   let value1 = prompt("Enter Start of the Range.");
+//   let value2 = prompt("Enter End Of the Range.");
   
-  //just change the Data type of the perticular 
-  value1= isNaN(value1)?value1:parseInt(value1);
+//   //just change the Data type of the perticular 
+//   value1= isNaN(value1)?value1:parseInt(value1);
 
-  value2= isNaN(value2)?value2:parseInt(value2);
-  alert(isSameType(value1, value2));
+//   value2= isNaN(value2)?value2:parseInt(value2);
+//   alert(isSameType(value1, value2));
+
+
+
+let value1 = prompt('Enter a value1')
+let value2 = prompt('Enter a value2')
+if(typeof value1 === typeof value2){
+ console.log("True");
+}
+else{
+console.log("False");
+}
   
